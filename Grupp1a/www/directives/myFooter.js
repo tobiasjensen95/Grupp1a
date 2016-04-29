073cbe9bd353c889437addd747fc0d587e37d6c6
Sebastian Kellgren <Sebastian Kellgren>
2016-04-29 16:10:39 +0200

    footer

---
+++ Grupp1a/www/directives/myFooter.js
@@ -0,0 +1,9 @@
+app.directive('myFooter', [function(){
+
+  return {
+    templateUrl: '/directives/myFooter.html',
+    controller: ['$scope', '$interval', function($scope, $interval) {   
+
+    }]
+  };
+}]);
