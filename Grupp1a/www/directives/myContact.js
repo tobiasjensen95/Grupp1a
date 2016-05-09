79def0cb479b3949ef67f33659b9ba07179cabd8
Sebastian Kellgren <Sebastian Kellgren>
2016-05-09 14:13:09 +0200

    blandat

---
+++ Grupp1a/www/directives/myContact.js
@@ -0,0 +1,9 @@
+app.directive('myContact', [function(){
+
+  return {
+    templateUrl: '/directives/myContact.html',
+    controller: ['$scope', '$interval', function($scope, $interval) {
+        
+    }]
+  };
+}]);
