f8a6012981e3bed82327046769e203fb049335ea
Tobias Jensen <tobias.jensen45@gmail.com>
2016-05-22 15:19:47 +0200

    bokning

---
+++ Grupp1a/www/directives/datepicker.js
@@ -0,0 +1,16 @@
+app.directive('datepicker', [function() {
+
+  return {
+    templateUrl: '/directives/datepicker.html',
+    link: function(scope, elem, attrs) {
+      elem.find('.dateInputField').datepicker({
+        dateFormat: "yy-mm-dd"
+      });
+    },
+    controller: ['$scope', function($scope) {
+      $scope.submitForm = function() {
+        console.log("$scope.selectedDate:", $scope.selectedDate);
+      };
+    }]
+  };
+}]);
