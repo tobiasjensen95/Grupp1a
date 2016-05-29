45e0a285e8c120dd1ea9cbb855eaea302aace527
Sebastian Kellgren <Sebastian Kellgren>
2016-05-29 12:58:54 +0200

    rensning

--- Grupp1a/www/directives/bostad-detail.js
+++
@@ -1,5 +0,0 @@
-/*app.controller("bostadDetail", ["$scope","$routeParams", "Bostad", function($scope, $routeParams, Bostad) {
-
-  $scope.bostad = Bostad.getById({id:$routeParams.id});
- 
-}]);*/
--- Grupp1a/www/directives/searchnavbar.js
+++
@@ -1,9 +0,0 @@
-app.directive('searchnavbar', [function(){
-
-  return {
-    templateUrl: '/directives/searchnavbar.html',
-    controller: ['$scope', '$interval', function($scope, $interval) { 
-    
-    }]
-  };
-}]);
